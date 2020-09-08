39ee2d3adb7f2b9fcdd71a59a9d6113b02d48436
Ivan-Bogdan <vanja.zeht2013@gmail.com>
2020-09-08 17:37:25 +0300

    Updates

---
+++ precache-manifest.8e3fc728406681658364131b5c8d8d3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a64b3933d1c1864595488e2ec95e7b5",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "88a8711d087c473449f7",
+    "url": "/task/static/css/main.5c957229.chunk.css"
+  },
+  {
+    "revision": "4e68bbeae047e932d3f1",
+    "url": "/task/static/js/2.43fa7c35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task/static/js/2.43fa7c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a8711d087c473449f7",
+    "url": "/task/static/js/main.8f08bcb2.chunk.js"
+  },
+  {
+    "revision": "8d3b7af28276639cef39",
+    "url": "/task/static/js/runtime-main.e553ee44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/task/static/media/logo.5d5d9eef.svg"
+  }
+]);
